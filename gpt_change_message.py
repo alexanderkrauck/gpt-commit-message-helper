5db42f759b579cbf0748911a02ffaa621fac5925
Alexander Krauck <alexander.krauck@gmail.com>
2023-06-01 21:52:01 +0200

    Enabled Multiversion comparison

--- gpt_change_message.py
+++ gpt_change_message.py
@@ -4,7 +4,8 @@ from os.path import join, basename
 import sys
 import subprocess
 from git import Repo
-from requests.exceptions import RequestException
+from openai.error import RateLimitError
+from typing import List, Optional, Tuple
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
@@ -14,31 +15,52 @@ openai_model = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
 openai_temperature = float(os.getenv("OPENAI_TEMPERATURE", "0.7"))
 # openai_stop = os.getenv("OPENAI_STOP", None)
 
-
-def get_staged_files(repo_path):
-    repo = Repo(repo_path)
-    return [item.a_path for item in repo.index.diff("HEAD")]
-
-
-def get_file_contents_before_staging(repo_path, file_path):
+def get_changed_files(repo_path, before_version, after_version):
     repo = Repo(repo_path)
 
-    # Get the blob (file) from the last commit
-    blob = repo.head.commit.tree[file_path]
+    if after_version == "STAGED":
+        commit2 = repo.index
+    else:
+        commit2 = repo.commit(after_version)
 
-    # Get the file contents
-    return blob.data_stream.read().decode("utf-8")
+    commit1 = repo.commit(before_version)
+
+    diffs = commit2.diff(commit1)
+
+    changed_files = [item.a_path for item in diffs]
+
+    return changed_files
+
 
 
 def get_current_file_contents(repo_path, file_path):
-    with open(join(repo_path, file_path), "r") as file:
-        return file.read()
+    try:
+        with open(join(repo_path, file_path), "r") as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File does not exist in this version of the repository."
 
 
 def remove_first_line(s):
     lines = s.split("\n")
     lines.pop(0)
     return "\n".join(lines)
+
+
+def get_file_contents(repo_path, file_path, version):
+    repo = Repo(repo_path)
+
+    if version == "STAGED":
+        return get_current_file_contents(repo_path, file_path)
+
+    # Get the blob (file) from the last commit
+    try:
+        blob = repo.commit(version).tree[file_path]
+    except KeyError:
+        return "File does not exist in this version of the repository."
+
+    # Get the file contents
+    return blob.data_stream.read().decode("utf-8")
 
 
 def generate_summary(file_path):
@@ -73,15 +95,45 @@ def generate_summary(file_path):
     return summary.strip()
 
 
-def generate_change_message(repo_path, return_prompts=False):
+def generate_change_message(
+    repo_path: str,
+    return_prompts: Optional[bool] = False,
+    version_before: Optional[str] = "HEAD",
+    version_after: Optional[str] = "STAGED",
+):
+    """
+    Generate a summary of the changes made between two versions of a repository.
 
-    
+    Parameters
+    ----------
+    repo_path : str
+        Path to the repository.
+    return_prompts : bool, optional
+        Whether to return the prompts used to generate the commit message.
+    version_before : str, optional
+        Version to compare to. Defaults to "HEAD". See below for more information.
+    version_after : str, optional
+        Version to compare from. Defaults to "HEAD". See below for more information.
+
+    Options for version_before and version_after:
+    - "HEAD": The current (commited) version of the repository.
+    - "STAGED": The staged version of the repository.
+    - "HEAD~n": The version of the repository n commits ago.
+    - A commit hash: The version of the repository at the given commit hash.
+    """
+
+    assert version_before != "STAGED", "version_before cannot be 'STAGED'"
+    assert (
+        version_before != version_after
+    ), "version_before and version_after cannot be the same"
+
     promts = []
     file_texts = {}
-    file_paths = get_staged_files(repo_path)
+
+    file_paths = get_changed_files(repo_path, version_before, version_after)
     for file_path in file_paths:
-        file_contents_before = get_file_contents_before_staging(repo_path, file_path)
-        file_contents_after = get_current_file_contents(repo_path, file_path)
+        file_contents_before = get_file_contents(repo_path, file_path, version_before)
+        file_contents_after = get_file_contents(repo_path, file_path, version_after)
 
         query_comparison = (
             "Those are the changes of a file "
@@ -94,18 +146,22 @@ def generate_change_message(repo_path, return_prompts=False):
         promt_comparison = f"{query_comparison}\n\n\nBEFORE CHANGES:\n{file_contents_before}\n\n\nAFTER CHANGES:\n{file_contents_after}"
         promts.append(promt_comparison)
 
-        response_text_comparison = (
-            openai.ChatCompletion.create(
-                temperature=openai_temperature,
-                model=openai_model,
-                messages=[
-                    {"role": "system", "content": "You are a project manager."},
-                    {"role": "user", "content": promt_comparison},
-                ],
+        try:
+            response_text_comparison = (
+                openai.ChatCompletion.create(
+                    temperature=openai_temperature,
+                    model=openai_model,
+                    messages=[
+                        {"role": "system", "content": "You are a project manager."},
+                        {"role": "user", "content": promt_comparison},
+                    ],
+                )
+                .choices[0]["message"]["content"]
+                .strip()
             )
-            .choices[0]["message"]["content"]
-            .strip()
-        )
+        except RateLimitError:
+            print("Rate Limit Exceeded")
+            break
 
         query_refine = (
             "Reformulate the following so that it doesn't seem as if the writer "
@@ -117,22 +173,26 @@ def generate_change_message(repo_path, return_prompts=False):
         promt_refine = f"{query_refine}\n\n{response_text_comparison}"
         promts.append(promt_refine)
 
-        response_text_refined = (
-            openai.ChatCompletion.create(
-                temperature=openai_temperature,
-                model=openai_model,
-                messages=[
-                    {
-                        "role": "system",
-                        "content": "You are an machine learning software engineer.",
-                    },
-                    {"role": "user", "content": promt_refine},
-                ],
+        try:
+            response_text_refined = (
+                openai.ChatCompletion.create(
+                    temperature=openai_temperature,
+                    model=openai_model,
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": "You are an machine learning software engineer.",
+                        },
+                        {"role": "user", "content": promt_refine},
+                    ],
+                )
+                .choices[0]["message"]["content"]
+                .strip()
             )
-            .choices[0]["message"]["content"]
-            .strip()
-        )
-
+        except RateLimitError:
+            print("Rate Limit Exceeded")
+            break
+        
         response_text_refined = remove_first_line(response_text_refined)
         file_texts[file_path] = response_text_refined
 
--- scripts/ipy_use_commit_message.py
+++ scripts/ipy_use_commit_message.py
@@ -5,7 +5,7 @@ os.chdir("..")
 import gpt_change_message
 
 #%%
-commit_message, promts = gpt_change_message.generate_change_message(".", return_prompts=True)
+commit_message, promts = gpt_change_message.generate_change_message(".", return_prompts=True, version_before="HEAD~5", version_after="STAGED")
 #print(promt)
 print(commit_message)
 
